842e068dcf075db5451799be5dc6269aa5ef0973
maitreyee <maitreyeetewari.24@gmail.com>
2019-06-29 15:09:22 +0200

    #pattern extraction

---
+++ code/dialogue_act_classifier/classifier_da.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from sklearn.decomposition import PCA
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from gensim.models import KeyedVectors
+import numpy as np
+from dialog_acts import DialogueActSample
+import pandas as pd
+import nltk
+
+class DaClassifier():
+    def __init__(self):
+        #in the terminal load the glove txt file and save it
+        #we = KeyedVectors.load_word2vec_format('/home/maitreyee/Development/glove/glove.6B.300d.txt')
+        #we.save_word2vec_format('/home/maitreyee/Development/glove/glove.6B.300d.bin', binary=True)
+        self.wrdembedding = KeyedVectors.load_word2vec_format('/home/maitreyee/Development/glove/glove.6B.300d.bin',
+                                                              binary=True)
+
+        self.classes_int_encoder = LabelEncoder()
+
+        self.classes_encoder = OneHotEncoder()
+
+        self.utt_we, self.acts_de = [], []
+
+        self.classifier = Sequential([Dense(units=300, activation='relu'),  # input_dim=100-n_removed_cmps,
+                                      Dropout(.5),
+                                      Dense(units=250, activation='relu'),
+                                      Dropout(.5),
+                                      Dense(units=22, activation='sigmoid')])
+
+    #encodes the classes in the dataset, encoding here:we encode the 20 classes into n-1 or 19 classes.
+    # Becuase the classes start at 0. (-1 says that first dimnesion is not
+    # known and the second states that the array should be one dimnesion.)
+
+    def classesEncod(self,sampled_data):
+        resampled_annotate = sampled_data
+        acts = [str(a).lower().strip() for a in resampled_annotate.commfunct.values]
+        classes_int = self.classes_int_encoder.fit_transform(acts).reshape(-1, 1)
+        classes = self.classes_encoder.fit_transform(classes_int).toarray()
+        return classes
+
+    #for graphical presentation of data that extract the principle values from the numerical dataset.
+    def pcaTransform(self):
+        backup_vectors = self.wrdembedding.vectors
+        pca = PCA()
+        n_removed_cmps = 1
+        vectors_pca = pca.fit_transform(backup_vectors)
+        vectors_pca[:, :n_removed_cmps] = 0
+        self.wrdembedding.vectors = pca.inverse_transform(vectors_pca)
+    # create a mean value of the weigted array.
+    def average_words(self, tokens, noise=0.):
+
+        tokens = [t.strip(')( ., ?') for t in tokens]
+
+        if noise <= 0.:
+            we = [self.wrdembedding[w].reshape(1,-1) for w in tokens if w in self.wrdembedding]
+        else:
+            we = []
+            for t in tokens:
+                if np.random.uniform(size=1) <= noise:
+                    idx = np.random.choice(self.wrdembedding.vectors.shape[0])
+                    we.append(self.wrdembedding.vectors[idx, :])
+                else:
+                    if t in self.wrdembedding:
+                        we.append(self.wrdembedding[t])
+
+        if len(we) > 0:
+            mean_we = np.mean(we, axis=0, keepdims=True)
+            return mean_we
+        else:
+            return None
+    #generate the train and test samples for x i.e utterances and y i.e classes
+    def traintestgenerate(self, df_cleaned):
+        classesx = self.classesEncod(df_cleaned)
+        utt = df_cleaned.utterance.values
+        for i in range(len(utt)):
+            u = utt[i]
+            a = classesx[i]
+            tokens = str(u).strip().lower().split()
+            mean_we = self.average_words(tokens, noise=.15)
+
+            if mean_we is None:
+                continue
+
+            if mean_we.shape == (1, 1):
+                continue
+            self.utt_we = self.utt_we + [mean_we]
+            self.acts_de = self.acts_de + [a]
+
+        self.acts_de = np.vstack(self.acts_de)
+        self.utt_we = np.vstack(self.utt_we)
+
+        size_utt = self.utt_we.shape[1]
+
+        dataset = np.hstack([self.utt_we, self.acts_de])
+        np.random.shuffle(dataset)
+
+        self.utt_we, self.acts_de = dataset[:, :size_utt], dataset[:, size_utt:]
+        perc_train = .5
+
+        idx_train = int(self.utt_we.shape[0] * perc_train)
+
+        x_train, x_test = self.utt_we[:idx_train, :], self.utt_we[idx_train:, :]
+
+        y_train, y_test = self.acts_de[:idx_train, :], self.acts_de[idx_train:, :]
+        yield x_train,x_test,y_train,y_test
+    #perform the classification and print the log, some metrics to perform the classification is provided here
+    #the classification neural layer is defined in the __init__ function.
+    def classifier_n(self, frmtraintest):
+        data_toClassify = frmtraintest
+
+        # We compile it
+        self.classifier.compile(optimizer=Adam(1e-4),
+                           loss='categorical_crossentropy',
+                           metrics=['accuracy'])
+        for xytrain in data_toClassify:
+            x_train, x_test, y_train,y_test = xytrain
+        # We fit the network
+            h = self.classifier.fit(x=x_train,
+                               y=y_train,
+                               validation_data=[x_test, y_test],
+                               epochs=100,
+                               batch_size=10,
+                               verbose=2,
+                               callbacks=[])
+        # its for logging, and showing results
+
+    #analyse the results of the neural network
+    def confmatrx(self, xandytest):
+        for traintest in xandytest:
+            x_train, x_test, y_train, y_test = traintest
+            y_true = y_test
+            y_pred = self.classifier.predict(x_test)
+
+            idx_max_true = np.argmax(y_true, axis=1)
+            idx_max_pred = np.argmax(y_pred, axis=1)
+
+            pd.options.display.max_columns = 500
+
+            conf_mat = confusion_matrix(idx_max_true, idx_max_pred)
+            np.core.arrayprint._line_width = 160
+            print(conf_mat, )
+
+    def test_on_brkdown(self):
+        test_file = []
+        utt_test = []
+        with open('/home/maitreyee/Development/data/brkdwn1.txt', 'r') as f:
+            f = f.read()
+            stripped = f.strip()
+            test_file.append(stripped)
+
+        sents = test_file[0].split("\n")
+        sents1 = [t.strip(' ,.') for t in sents]
+
+        for i in range(len(sents1)):
+            u = sents1[i]
+            tokens = str(u).lower().split()
+            we = [self.wrdembedding[w] for w in tokens if w in self.wrdembedding]
+            mean_we = np.mean(we, axis=0, keepdims=True).reshape(1, -1)
+            if mean_we.shape == (1, 1):
+                continue
+            utt_test = utt_test + [mean_we]
+
+        utt_test = np.concatenate(utt_test, axis=0)
+        utt_test = np.vstack(utt_test)
+        res = self.classifier.predict(utt_test)
+        pred_class = [list(zip(*[(a, r[a]) for a in range(len(r)) if r[a] > .20])) for r in res]
+        res = [list(zip(self.classes_int_encoder.inverse_transform(np.array(p[0], dtype='int')), p[1])) for p in pred_class
+               if len(p) > 0]
+        p_res = list(zip(sents, res))
+        df_das = pd.DataFrame(p_res)
+        print(df_das.head(20))
+
+if __name__ == '__main__':
+    sampled_data = DialogueActSample()
+    df_cleaned1 = pd.read_csv('./cleaned.csv',sep='\t')
+    sampling = sampled_data.samplingFeatures(df_cleaned1)
+    classifier = DaClassifier()
+    classes = classifier.classesEncod(sampling)
+    traintest = classifier.traintestgenerate(sampling)
+    logging_classifier = classifier.classifier_n(traintest)
+    confmat = classifier.confmatrx(traintest)
+    print(confmat)
---
+++ code/dialogue_act_classifier/pattern_classification.py
@@ -0,0 +1,113 @@
+import pandas as pd
+from features_extract import TopicExtractDep
+from nltk import ngrams
+from sklearn.feature_extraction.text import TfidfVectorizer
+#from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.cluster.hierarchy import ward
+from sklearn.cluster import AgglomerativeClustering
+import hdbscan
+#import matplotlib.pyplot as plt
+from sklearn import metrics
+from gensim.models import KeyedVectors
+import numpy as np
+
+
+class BrkdownClassifier():
+
+
+    def __init__(self):
+        self.wrdembedding = KeyedVectors.load_word2vec_format('/home/maitreyee/Development/glove/glove.6B.300d.bin',
+                                                              binary=True)
+
+        self.brkdown_corpora1 = pd.read_csv('/home/maitreyee/Development'
+                                            '/Dm_develop/code/dialogue_act_classifier/brkdown_corpora1.csv',
+                                           sep ='\t')
+        utterances = list(ngrams(self.brkdown_corpora1['utterance'].astype(str),2))
+
+        top_extr = TopicExtractDep()
+        self.extract_feats = top_extr.nouns_extract(utterances)
+        self.datadf = pd.read_csv('./plotdata.csv', sep ='\t')
+        self.uttwe = []
+
+
+    def vectorize_data(self):
+        tfidf_vect = TfidfVectorizer(max_df=0.8, max_features=1000,
+                                     min_df=0.01, use_idf=True,
+                                     ngram_range=(1, 2))
+        self.datadf['nouns_kewords'] = self.datadf['noun']+' '+self.datadf['keywords']
+        feats3 = list(self.datadf.nouns_kewords.astype(str))
+        tfidf_matrix1 = tfidf_vect.fit_transform(feats3)
+
+        tfidf_array = tfidf_matrix1.toarray()
+        cosine_dist = cosine_similarity(tfidf_matrix1)
+        self.linkage_mat_v1v2 = ward(cosine_dist)
+        return zip(self.linkage_mat_v1v2, tfidf_array)
+
+
+    def word2vec(self):
+        self.datadf.combined = self.datadf.noun +' '+self.datadf.keywords+' '+self.datadf.subobjvrb
+        utt1 = self.datadf.combined
+        for i in range(len(utt1)):
+            u = utt1[i]
+            tokens = str(u).strip().lower().split()
+            we = [self.wrdembedding[w] for w in tokens if w in self.wrdembedding]
+            mean_we = np.mean(we, axis=0, keepdims=True).reshape(1, -1)
+            if mean_we.shape == (1, 1):
+                continue
+            self.uttwe = self.uttwe + [mean_we]
+            X = np.array(self.uttwe)
+            return X
+
+
+    def hierarchical_clustering(self):
+        tfidf_data = self.vectorize_data()
+        wrdemd_data = self.word2vec()
+        for matrixplotarray in tfidf_data:
+            matrix, tfidf_array = matrixplotarray
+            clusterAglo_tfidf = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='complete')
+            clusterAglo_tfidf.fit_predict(matrix)
+            clusterAglo_wrdembd = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='complete')
+            clusterAglo_wrdembd.fit_predict(wrdemd_data)
+            return zip(clusterAglo_tfidf, clusterAglo_wrdembd)
+            #plt.scatter(tfidf_array[:, 0], tfidf_array[:, 1],
+             #       c=clusterAglo.labels_, cmap='rainbow', s=15, alpha=1)
+    def density_clutering(self):
+        X = self.word2vec()
+        X = np.array(X)
+        wrd_Embd = X.reshape(-1,1)
+        tfidf_data = self.vectorize_data()
+        clusterHdbscn_tfidf = hdbscan.HDBSCAN(min_cluster_size=2)
+        clusterHdbscn_wrdembd = hdbscan.HDBSCAN(min_cluster_size=2)
+        clusterHdbscn_wrdembd.fit_predict(wrd_Embd)
+        for xy in tfidf_data:
+            matrix, array = xy
+            clusterHdbscn_tfidf = clusterHdbscn_tfidf.fit_predict(matrix)
+
+            yield clusterHdbscn_tfidf
+
+        yield clusterHdbscn_wrdembd
+
+    def evaluation(self):
+        Agglocluster_result = self.hierarchical_clustering()
+        vector_data = self.vectorize_data()
+        for matrixplotarray in vector_data:
+            matrix, tfidf_array = matrixplotarray
+            for tfidfwrdembd in Agglocluster_result:
+                cluster_tfidf, cluster_wrdembd = tfidfwrdembd
+                labels_tfidf = cluster_tfidf.labels_
+                labels_wrdembd = cluster_wrdembd.labels_
+                classes = list(self.datadf.nouns_kewords)
+                print("Silhouette Score is {}, "
+                      "and calinski score is {}".format(#metrics.homogeneity_score(classes[0:], labels),
+                                       #   metrics.completeness_score(classes, labels),
+                                       #   metrics.v_measure_score(classes,labels),
+                                          metrics.calinski_harabasz_score(matrix,labels_tfidf),
+                                          metrics.calinski_harabasz_score(matrix,labels_wrdembd)))
+
+
+if __name__ == '__main__':
+    classifier = BrkdownClassifier()
+    cluster = classifier.evaluation()
+
+
